5a8bc40167d12641e4cdedaa934e5a7e935a6224
FelpsDev12 <estevamfelipe862@gmail.com>
2025-10-18 16:47:45 -0300

    Delete models directory

--- models/Notas.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const NoteSchema = new mongoose.Schema({
-    title: {type: String, required: true, unique: false},
-    note: {type: String, require: true, unique: false},
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
-}, {timestamps: true});
-
-module.exports = mongoose.model('Notas', NoteSchema);
